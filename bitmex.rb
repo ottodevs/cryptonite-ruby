6c270b9948354fce1cf02a47605b036c2b312abd
David Krmpotic <david.krmpotic@gmail.com>
2015-11-03 21:54:37 +0100

    add bitmex

---
+++ bitmex.rb
@@ -0,0 +1,34 @@
+require 'faraday'
+require 'faraday_middleware'
+
+class Bitmex
+
+  API_BASE = "https://www.bitmex.com/api/v1/"
+
+  def mark_price
+    @mark_price ||= conn.get('instrument/indices').body[0]['markPrice']
+  end
+
+  def bid_price
+    '%.2f' % bid_ask['bidPrice'].to_f
+  end
+
+  def ask_price
+    '%.2f' % bid_ask['askPrice'].to_f
+  end
+
+private
+
+  def bid_ask
+    @bid_ask ||= conn.get('orderBook', { symbol: 'XBT', depth: 1 }).body[0]
+  end
+
+  def conn
+    @conn ||= Faraday.new(API_BASE) do |conn|
+      conn.request :json
+      conn.response :json, :content_type => /\bjson$/
+      conn.adapter Faraday.default_adapter
+    end
+  end
+
+end
--- prices.rb
+++ prices.rb
@@ -2,9 +2,19 @@
 require 'colorize'
 require_relative 'bitstamp'
 require_relative 'kraken'
+require_relative 'bitmex'
 
 bitstamp = Bitstamp.new
-puts "BTC:".green + " $#{bitstamp.price_usd} / #{bitstamp.price_eur}€"
+puts "Bitstamp:".green + " $#{bitstamp.price_usd} / #{bitstamp.price_eur}€"
+
+bitmex = Bitmex.new
+puts
+puts "Bitmex".green
+puts "Mark: $#{bitmex.mark_price}"
+puts "Bid : $#{bitmex.bid_price}"
+puts "Ask : $#{bitmex.ask_price}"
+
+puts
 
 kraken = Kraken.new
-puts "ETH:".green + " $#{kraken.price_usd} / #{kraken.price_eur}€"
+puts "ETH (Kraken):".green + " $#{kraken.price_usd} / #{kraken.price_eur}€"
